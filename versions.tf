261e4ca4cc41a6fe771d8a9c196a081a589ff1c6
Vijay Veeranki <vijay.veeranki@digital.justice.gov.uk>
2023-03-02 15:44:50 +0000

    Add kubectl provider

--- versions.tf
+++ versions.tf
@@ -8,6 +8,10 @@ terraform {
       source  = "hashicorp/kubernetes"
       version = ">=2.0.0"
     }
+    kubectl = {
+      source  = "gavinbunney/kubectl"
+      version = "1.13.2"
+    }
   }
   required_version = ">= 0.14"
 }
