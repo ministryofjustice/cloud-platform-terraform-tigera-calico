a71f1210c47069c0ccad2d5c5a3396f101a9005e
jaskaransarkaria <jaskaran.sarkaria@digital.justice.gov.uk>
2023-03-03 15:54:39 +0000

    fix: 🐛 patch service account finalizers

--- calico.tf
+++ calico.tf
@@ -119,6 +119,8 @@ resource "null_resource" "remove_installation" {
     command = <<-EOT
       kubectl patch -n calico-system installations.operator.tigera.io/default --type json --patch='[{"op":"remove","path":"/metadata/finalizers"}]'
       kubectl delete installations.operator.tigera.io default
+      kubectl patch -n tigera-operator serviceaccount --type json --patch='[{"op":"remove","path":"/metadata/finalizers"}]'
+      kubectl patch -n calico-system serviceaccount --type json --patch='[{"op":"remove","path":"/metadata/finalizers"}]'
     EOT
     on_failure = continue
   }
