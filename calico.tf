86e285f0fb654f521025733be97ea31d084b8fa3
tom-j-smith <thomas.smith@digital.justice.gov.uk>
2025-07-02 17:07:47 +0100

    Setting to higher priority

--- calico.tf
+++ calico.tf
@@ -10,7 +10,7 @@ kind: GlobalNetworkPolicy
 metadata:
   name: deny-aws-imds
 spec:
-  order: 500
+  order: 5000
   selector: projectcalico.org/namespace not in { "cert-manager", "ingress-controllers", "kube-system", "logging", "monitoring", "velero" }
   types:
     - Egress
