904cc8577f4a2d5a16bbae19987a7ef43c2d6b0b
tom-j-smith <thomas.smith@digital.justice.gov.uk>
2025-07-03 15:41:28 +0100

    Up calico version

--- calico.tf
+++ calico.tf
@@ -160,7 +160,7 @@ resource "helm_release" "tigera_calico" {
   repository = "https://projectcalico.docs.tigera.io/charts"
   namespace  = "tigera-operator"
   timeout    = 300
-  version    = "3.28.1"
+  version    = "3.29.0"
   skip_crds  = true
 
   set {
